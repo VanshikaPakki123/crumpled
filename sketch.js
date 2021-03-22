b6684047ad3b2b432854866777554480ed55e15a
VanshikaPakki123 <76252285+VanshikaPakki123@users.noreply.github.com>
2021-03-22 09:27:08 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+
+function preload() 
+{
+	PaperImg = loadImage("paper.png");
+}
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+
+	dustbinObj=new dustbin(1200,650);
+
+	Paper=new paper(100,100,100,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  Paper.display();
+
+}
+
+function keyPressed() {
+
+	if(keyCode === RIGHT_ARROW){
+           Paper.x = Paper.x + 20;
+	}
+	
+        if(keyCode === LEFT_ARROW){
+			Paper.x = Paper.x - 20;
+	}
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(PaperBody,false); 
+	}
+}
